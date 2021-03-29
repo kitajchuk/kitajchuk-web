2f5c431ece56ead6921844f5a7f83af52863057e
kitajchuk <bk@kitajchuk.com>
2021-03-28 19:40:02 -0700

    FlamingOs stub...

---
+++ pages/flamingos.js
@@ -0,0 +1,42 @@
+import Layout from '../components/layout';
+import { createDecks } from '../lib/flamingos/tools';
+import { useEffect, useState } from 'react';
+
+const FlamingOs = () => {
+  const [decks, setDecks] = useState(null);
+
+  useEffect(() => {
+    if (!decks) {
+      setDecks(createDecks());
+
+    } else {
+      console.log(decks);
+    }
+  });
+
+  const onClickShuffle = () => {
+    setDecks(createDecks());
+  };
+
+  return (
+    <Layout title="FlamingOs">
+      {decks && Object.keys(decks).map((key) => {
+        return (
+          <>
+            <div className="ctrls">
+              <button onClick={onClickShuffle}>Shuffle Decks</button>
+            </div>
+            <div className="deck" key={key}>
+              <img src={decks[key].back} key="back" />
+              {decks[key].cards.map((card) => {
+                return <img src={card.asset} key={card.id} />
+              })}
+            </div>
+          </>
+        );
+      })}
+    </Layout>
+  )
+};
+
+export default FlamingOs;
