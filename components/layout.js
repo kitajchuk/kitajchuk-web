48bd8270ea0180aea3f46b5afe182a5c6d7a635d
kitajchuk <bk@kitajchuk.com>
2021-04-01 16:53:43 -0700

    Layout structure

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,32 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import Logo from '../components/logo';
+import { nanoid } from 'nanoid';
+
+const navi = [
+  {
+    link: '/flamingos',
+    label: 'FlamingOs',
+  },
+];
+
+const footer = [
+  {
+    link: 'mailto:bk@kitajchuk.com',
+    label: 'Email',
+    open: true,
+  },
+  {
+    link: 'https://github.com/kitajchuk',
+    label: 'Github',
+    open: true,
+  },
+  {
+    link: 'https://www.linkedin.com/in/brandonleekitajchuk/',
+    label: 'Linkedin',
+    open: true,
+  },
+];
 
 export default function Layout({children, title = "kitajchuk"}) {
   return (
@@ -20,7 +48,38 @@ export default function Layout({children, title = "kitajchuk"}) {
         */}
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      {children}
+      <header className="head flex justify-center items-center">
+        <Link href="/">
+          <a>
+            <Logo fill="#000" />
+          </a>
+        </Link>
+      </header>
+      <nav className="navi text-black text-center">
+        {navi.map((nav) => {
+          return (
+            <div key={nanoid()}>
+              <Link href={nav.link}>
+                <a target={nav.open ? '_blank' : null}>{nav.label}</a>
+              </Link>
+            </div>
+          );
+        })}
+      </nav>
+      <main className="main">
+        {children}
+      </main>
+      <footer className="navi text-black text-center">
+        {footer.map((nav) => {
+          return (
+            <div key={nanoid()}>
+              <Link href={nav.link}>
+                <a target={nav.open ? '_blank' : null}>{nav.label}</a>
+              </Link>
+            </div>
+          );
+        })}
+      </footer>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,11 @@
-// import classNames from 'classnames';
-import Link from 'next/link';
 import Layout from '../components/layout';
-import Logo from '../components/logo';
 
 export default function Home() {
   return (
     <Layout>
-      <header className="navi flex justify-center items-center">
-        <Logo fill="#000" />
-      </header>
-      <main className="main text-black text-center">
-        <div><Link href="mailto:bk@kitajchuk.com"><a target="_blank">Email</a></Link></div>
-        <div><Link href="https://github.com/kitajchuk"><a target="_blank">Github</a></Link></div>
-        <div><Link href="https://www.linkedin.com/in/brandonleekitajchuk/"><a target="_blank">Linkedin</a></Link></div>
-      </main>
-      <footer className="hero flex justify-center items-center">
+      <div className="hero flex justify-center items-center">
         <img src="/img/kitajchuk_hero.png" />
-      </footer>
+      </div>
     </Layout>
   )
 };
