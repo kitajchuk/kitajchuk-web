984d46dd1f49c25678a06c416db66f6605367c45
kitajchuk <bk@kitajchuk.com>
2021-05-14 07:50:31 -0700

    Use next/image for homepage and 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,31 @@
-import Layout from '../components/layout';
+import Image from 'next/image';
 
-export default function Page404() {
+import Layout from '../components/layout';
+import { getDesiredSize } from '../lib/utils';
+
+export default function Page404({size}) {
   return (
     <Layout>
       <div className="hero">
-        <img src="/img/kitajchuk_hollow.png" />
+        <Image
+          src="/img/kitajchuk_hollow.png"
+          width={size.width}
+          height={size.height}
+          layout="intrinsic"
+          priority={true}
+          loading="eager"
+        />
       </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const size = getDesiredSize(1303, 1920, 480);
+
+  return {
+    props: {
+      size,
+    },
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import Layout from '../components/layout';
+import Image from 'next/image';
 
-export default function Home() {
+import Layout from '../components/layout';
+import { getDesiredSize } from '../lib/utils';
+
+export default function Home({size}) {
   return (
     <Layout>
       <div className="hero">
-        <img src="/img/kitajchuk_hero.png" />
+        <Image
+          src="/img/kitajchuk_hero.png"
+          width={size.width}
+          height={size.height}
+          layout="intrinsic"
+          priority={true}
+          loading="eager"
+        />
       </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const size = getDesiredSize(1345, 1920, 480);
+
+  return {
+    props: {
+      size,
+    },
+  };
 }
