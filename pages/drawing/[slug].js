a6c3087c31b1b379a2a3464d0ee53248e9d8d877
kitajchuk <bk@kitajchuk.com>
2021-06-03 10:52:02 -0700

    Add paths list to bottom of drawings pages

--- pages/drawing/[slug].js
+++ pages/drawing/[slug].js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { nanoid } from 'nanoid';
 
 import Layout from '../../components/layout';
@@ -5,7 +7,7 @@ import { withImageLoader } from '../../components/asyncimage';
 import { Animate } from '../../components/animate';
 import { readPublicImageDirectory, getDrawingStaticPaths } from '../../lib/utils';
 
-export default withImageLoader(({collection}) => {
+export default withImageLoader(({collection, paths}) => {
   return (
     <Layout title={collection.title}>
       <div className="drawings">
@@ -13,7 +15,6 @@ export default withImageLoader(({collection}) => {
           <p className="title">{collection.title}</p>
           <ul className="collection">
             {collection.images.map((img) => {
-              // img { src, dims { width, height, type } }
               return (
                 <li key={nanoid()} className="collection__item">
                   <Animate>
@@ -24,6 +25,15 @@ export default withImageLoader(({collection}) => {
             })}
           </ul>
         </div>
+        {paths.map((obj) => {
+          return (
+            <Link key={obj.params.slug} href={`/drawing/${obj.params.slug}`}>
+              <a className="link">
+                {obj.params.slug}
+              </a>
+            </Link>
+          );
+        })}
       </div>
     </Layout>
   );
@@ -40,10 +50,12 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({params}) {
   const collection = readPublicImageDirectory(`drawings/${params.slug}`);
+  const paths = getDrawingStaticPaths().filter((obj) => obj.params.slug !== params.slug);
 
   return {
     props: {
       collection,
+      paths,
     },
   };
 }
