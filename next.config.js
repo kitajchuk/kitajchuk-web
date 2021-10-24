c167ad074f865c997d15c7c558359f2d987ea0f4
kitajchuk <bk@kitajchuk.com>
2021-10-23 20:12:42 -0700

    Adding layer0

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
-module.exports = {
-  trailingSlash: true,
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only for client
-    if (!isServer && !dev) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-  
-    return config;
-  },
-};
+const { withLayer0, withServiceWorker } = require('@layer0/next/config')
+
+module.exports = withLayer0(
+  withServiceWorker({
+    trailingSlash: true,
+    webpack: (config, { dev, isServer }) => {
+      // Replace React with Preact only for client
+      if (!isServer && !dev) {
+        Object.assign(config.resolve.alias, {
+          react: 'preact/compat',
+          'react-dom/test-utils': 'preact/test-utils',
+          'react-dom': 'preact/compat',
+        });
+      }
+    
+      return config;
+    },
+    layer0SourceMaps: true,
+  })
+);
