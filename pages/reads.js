34c8f083d23f7950f03476c848a0b9f5cd7df1f7
kitajchuk <bk@kitajchuk.com>
2021-10-23 20:37:31 -0700

    Add books to reads

--- pages/reads.js
+++ pages/reads.js
@@ -9,13 +9,20 @@ export default function Reads() {
   return (
     <Layout>
       <div className="navi books">
-        {books.map((book) => {
+        {Object.keys(books).map((key) => {
           return (
-            <div className="book" key={nanoid()}>
-              <Link href={`https://google.com/search?q=${book.title}&tbm=bks`}>
-                <a className="book__title" target="_blank">{book.title}</a>
-              </Link>
-              <div className="book__creds">{book.creds}</div>
+            <div className="books__type" key={key}>
+              <div className="books__label">{key}</div>
+              {books[key].map((book) => {
+                return (
+                  <div className="book" key={nanoid()}>
+                    <Link href={`https://google.com/search?q=${book.title}&tbm=bks`}>
+                      <a className="book__title" target="_blank">{book.title}</a>
+                    </Link>
+                    <div className="book__creds">{book.creds}</div>
+                  </div>
+                );
+              })}
             </div>
           );
         })}
