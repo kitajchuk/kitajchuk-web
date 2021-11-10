9e090abffc40b21b582555c95ad2c0c0923bc516
kitajchuk <bk@kitajchuk.com>
2021-11-10 08:47:05 -0800

    homepage image randomizer

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,36 @@
 import Layout from '../components/layout';
 import AsyncImage from '../components/asyncimage';
+import { getPublicStaticPaths, readPublicImageDirectory } from '../lib/utils';
 
-export default function Home() {
+export default function Home({images}) {
+  const shuffled = images.sort(() => 0.5 - Math.random());
+  const image = shuffled[Math.floor(Math.random() * images.length)];
+
   return (
     <Layout>
-      <div className="hero">
-        <AsyncImage src="/img/kitajchuk_hero.png" />
+      <div className="hero hero--home">
+        <AsyncImage src={image.src} />
       </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const paths = getPublicStaticPaths('drawings').filter((obj) => {
+    return /fallgirl|lost-kids/.test(obj.params.slug);
+  });
+  let images = [];
+
+  paths.forEach((obj) => {
+    const collection = readPublicImageDirectory(`drawings/${obj.params.slug}`);
+
+    images = images.concat(collection.images);
+  });
+
+  return {
+    props: {
+      paths,
+      images,
+    },
+  };
 }
