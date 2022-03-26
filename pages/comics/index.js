e9ccae82fa29904e919b5fd17c67998b6cdcd95d
kitajchuk <bk@kitajchuk.com>
2022-03-26 15:27:38 -0700

    comics update

--- pages/comics/index.js
+++ pages/comics/index.js
@@ -8,12 +8,12 @@ export default withImageLoader(({paths}) => {
   return (
     <Layout title="comics">
       <section className="comics__books">
-        <div>Free digital comics (PDF):</div>
+        <div>Tabi No Hana comics (PDF):</div>
         <div>
-          <a href="/media/Tabi_No_Hana_Book_01.pdf" target="_blank">Tabi No Hana: A Flower for Every Time, Book 1</a>
+          <a href="/media/Tabi_No_Hana_Book_01.pdf" target="_blank">A Flower for Every Time, Book 1</a>
         </div>
         <div>
-          <a href="/media/Tabi_No_Hana_Prequel_01.pdf" target="_blank">Tabi No Hana: A Day in Her Life, Prequel 1</a>
+          <a href="/media/Tabi_No_Hana_Prequel_01.pdf" target="_blank">A Day in Her Life, Prequel 1</a>
         </div>
       </section>
       <section className="comics__text">
