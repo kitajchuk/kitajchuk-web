6b5be59425f32f374d09ed84e34407bc76cbe392
Brandon Lee Kitajchuk <bk@kitajchuk.com>
2022-04-25 20:41:48 -0700

    Update dev.js

--- pages/dev.js
+++ pages/dev.js
@@ -29,7 +29,7 @@ export default function Dev() {
           </p>
         </section>
         <section className="dev__sqs uwrap colf">
-          <p className="pp m">I have a unique developer history with Squarespace from the time I worked at <a href="https://www.instrument.com/" target="_blank" rel="noreferrer">Instrument</a> and developed the <a href="https://github.com/kitajchuk/node-squarespace-server" target="_blank" rel="noreferrer">first open-source tools for the platform</a>. I made a lot of sqs sites, however I no longer do so because sqs is not a performant platform for serving sites.</p>
+          <p className="pp m">While working at <a href="https://www.instrument.com/" target="_blank" rel="noreferrer">Instrument</a> I developed the first <a href="https://github.com/kitajchuk/node-squarespace-server" target="_blank" rel="noreferrer">open-source dev tools for Squarespace</a>. I made a lot of sqs sites, however I no longer do so because I prefer the JAMstack now.</p>
           <p className="pp m">
             <a href="https://www.struck.com" target="_blank" rel="noreferrer">struck.com</a> | <a href="https://github.com/kitajchuk/struck-sqs" target="_blank" rel="noreferrer">github</a>
           </p>
@@ -47,7 +47,7 @@ export default function Dev() {
           </p>
         </section>
         <section className="dev__twitch uwrap colf">
-          <p className="pp m">I became an <a href="https://www.twitch.tv/kitajchuk" target="_blank" rel="noreferrer">Affiliate Twitch streamer</a> while making a comic book. I developed a custom stream HUD to gameify the stream for my chat. I also developed a Zelda and Pokemon themed labyrinth game using cellular automata to generate the mazes that my buddy <a href="https://www.twitch.tv/brucecooper" target="_blank" rel="noreferrer">@BruceCooper</a> used on his stream for awhile.</p>
+          <p className="pp m">I developed some bespoke, custom Twitch apps to gameify the stream for my <a href="https://www.twitch.tv/kitajchuk" target="_blank" rel="noreferrer">old affiliate channel</a> and for <a href="https://www.twitch.tv/brucecooper" target="_blank" rel="noreferrer">@BruceCooper</a>. The former is a zelda inspired game HUD and the latter was zelda/pokemon themed maze generated with cellular automata.</p>
           <p className="pp m">
             <a href="https://github.com/kitajchuk/twitch-hud" target="_blank" rel="noreferrer">twitch-hud</a>
           </p>
@@ -68,7 +68,7 @@ export default function Dev() {
           </p>
         </section>
         <section className="dev__legacy uwrap colf">
-          <p className="pp m">These are some older <a href="https://middlemanapp.com/" target="_blank" rel="noreferrer">middleman app</a> projects I built while at <a href="https://www.instrument.com/" target="_blank" rel="noreferrer">Instrument</a>. For posterity I have kept the static builds on Netlify. Rivals is especially dope on desktop. Check out the original <a href="https://legacy-maekan-prototype.netlify.app/kit-of-parts/">Kit of Parts</a> for MAEKAN to see how we would visualize the design elements in action.</p>
+          <p className="pp m">These are some older <a href="https://middlemanapp.com/" target="_blank" rel="noreferrer">middleman app</a> projects I built while at <a href="https://www.instrument.com/" target="_blank" rel="noreferrer">Instrument</a>. For posterity I have kept the static builds on Netlify. Rivals is especially cool on desktop. Check out the original <a href="https://legacy-maekan-prototype.netlify.app/kit-of-parts/">Kit of Parts</a> for MAEKAN to see how we would visualize the design elements in action.</p>
           <p className="pp m">
             <a href="https://legacy-familiar-rivals.netlify.app" target="_blank" rel="noreferrer">familiar rivals</a> | <a href="https://github.com/kitajchuk/legacy-familiar-rivals" target="_blank" rel="noreferrer">github</a>
           </p>
@@ -77,7 +77,7 @@ export default function Dev() {
           </p>
         </section>
         <section className="dev__nodesqs uwrap colf">
-          <p className="pp m">I built the first open-source Squarespace developer tools with nodejs. This culminated in a sqs engineer flying to Portland to interview me about it and ultimately said engineer developed <a href="https://developers.squarespace.com/tools" target="_blank" rel="noreferrer">official sqs dev tools</a> which I did some consulting on.</p>
+          <p className="pp m">I built the first open-source Squarespace dev tools with nodejs. A sqs engineer flew from New York to Portland to interview me about it and ultimately they developed <a href="https://developers.squarespace.com/tools" target="_blank" rel="noreferrer">official sqs dev tools</a> which I did some consulting on.</p>
           <p className="pp m">
             <a href="https://github.com/kitajchuk/node-squarespace-server" target="_blank" rel="noreferrer">node-squarespace-server</a>
           </p>
