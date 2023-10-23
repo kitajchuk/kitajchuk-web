a2f96eb1a4550728d768f04ebc5616ccd7bd0193
kitajchuk <bk@kitajchuk.com>
2023-10-22 19:06:30 -0700

    revert

--- pages/404.js
+++ pages/404.js
@@ -22,7 +22,7 @@ export default function Page404({ image }) {
 }
 
 export async function getStaticProps() {
-  const image = await getPublicImage("kitajchuk_hero.webp");
+  const image = await getPublicImage("kitajchuk_hollow.webp");
 
   return {
     props: {
